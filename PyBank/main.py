c92c7bcdffa3e5e9165be05cc8e7bf534e83f3d8
Daniel Atuesta <danielatuesta55@gmail.com>
2021-03-04 17:37:09 -0500

    Got file path to work

--- PyBank/main.py
+++ PyBank/main.py
@@ -4,37 +4,13 @@
 import os
 import csv
 
-#create the file path
-cvs_path = os.path.join('PyBank\Resources\budget_data.csv')
-#using the open with, create the path
-with open (cvs_path) as cvs_file:
+csv_path = os.path.join('Resources', 'budget_data.csv')
 
-    # Now that I have the file path I want to start cvs file handling
-    # I am specifying the variable that holds the contents 
-    cvs_reader = cvs.reader(cvs_file, delimiter = ',')
-    #print to see if path is running
-    print(cvs_reader)
-    #I need to direct python to read the header row first
-    header = next(cvsreader)
+with open(csvpath) as csvfile:
+    csv_reader = csv.reader(csvfile, delimiter=",")
 
-# Part 2: I need to tackle the first challange count the number of months in column 'DATE'
-
-    #Create a variable with the counter for the months and set it to 0
-    monthCounter = 0
-    # Create the empty list for months
-    months = []
+    for row in csv_reader:
+        print(row)
 
 
-    # I need to create a for loop that reads in each row of data after the header and writes the data into assigned lists
-    for row in cvs_reader:
-        #Assing the column 'Date' that is index 0 as month
-        month = row[0]
-        #using the append function add a list to a new list in this case I am adding list month to months to be able to later on count the number of items in the list
-        months.append(month)
-
-    # now outside the for loop I am going to count how many items in the list with the len function
-    monthCounter = len(months)
-     # I want to print monthcounter and check if the loop did what I wanted, after I check I need to set it as a comment so it dosent interrput the final python script.
-    print(monthCounter)
-    
 
